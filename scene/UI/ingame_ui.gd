555184a5b6cc443bf710e718a0af0022a8eea35f
Jon Fingas <jon.fingas@gmail.com>
2024-01-04 21:24:05 -0600

    added

--- scene/UI/ingame_ui.gd
+++ scene/UI/ingame_ui.gd
@@ -1,13 +1,17 @@
 extends Control
 
 @export var player: CharacterBody2D
+@export var UI_layer: CanvasLayer
 
+@onready var inventory = preload("res://scene/inventory.tscn")
 @onready var gemslabel = $"Collections/MarginContainer/Collections-Grid/Gems-Label"
 @onready var coinslabel = $"Collections/MarginContainer/Collections-Grid/Coins-Label"
 @onready var keyslabel = $"Collections/MarginContainer/Collections-Grid/Keys-Label"
 @onready var healthcontainer = $"Health/MarginContainer/VBoxContainer/HealthBar-Container"
 
 var healthScene = preload("res://scene/UI/heart.tscn")
+var inventory_access = false
+var instance
 
 func update_collections():
 	if player != null:
@@ -24,16 +28,15 @@ func update_health():
 	if current_health / 2 > current_hearts:
 		var diff = current_health / 2 - current_hearts
 		for x in range(0,diff):
-			var instance = healthScene.instantiate()
-			healthcontainer.add_child(instance)
-	elif current_health /2 < current_hearts:
+			var inst = healthScene.instantiate()
+			healthcontainer.add_child(inst)
+	elif current_health / 2 < current_hearts:
 		var diff = current_health / 2 - current_hearts
 		for x in range(diff,0):
 			var node = healthcontainer.get_child(x)
 			healthcontainer.remove_child(node)
 			node.queue_free()
 	
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	update_health()
@@ -41,5 +44,13 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
+	if not inventory_access and Input.is_action_just_pressed("inventory"):
+		instance = inventory.instantiate()
+		UI_layer.add_child(instance)
+		inventory_access = true
+	elif inventory_access and Input.is_action_just_pressed("inventory"):
+		inventory_access = false
+		instance.queue_free()
+		
 	update_health()
 	update_collections()
